c9a03d22eee55c45bf510076043ed373c7df9872
Martina Pauer <martinapauer38@gmail.com>
2026-05-19 15:22:02 +0000

    Add Function for calc musical intervals

--- src/modules/staff.py
+++ src/modules/staff.py
@@ -22,4 +22,45 @@ def get_note_text() -> str:
 
     natural_note += ' '
 
-    return natural_note         
+    return natural_note    
+
+def get_interval_note(grade: str, first_note: str) -> str:
+    '''
+        From Grade in Roman Numbers and type with the first interval note get the following using distances.
+    '''
+    distances: dict[str, str] = {
+                        'IIm': '1st', 
+                        'IIM': '2st', 
+                        'IIIm': '3st', 
+                        'IIIM': '4st', 
+                        'IVJ': '5st', 
+                        'Vdim': '6st', 
+                        'VJ': '7st', 
+                        'Vaug': '8st'
+                    }
+    natural_notes: list[str] = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+	# Use only note name, not the pitch
+    second_note: str = first_note[0]
+	# Calc Semitones to Use
+    semitones: str = distances[grade]
+    # Save the pitch for modify
+    octave: int = int(first_note[1])
+    
+    for note in range(natural_notes.index(first_note[0]), natural_notes + 1):
+         # Use next note until get the note after all semitones
+         second_note = natural_notes[note]
+         # Discount notes, when get zero restart for loop
+         if semitones == '0st':
+            second_note = f'{natural_notes[note]}{octave}'
+            break
+         elif ['C', 'D', 'F', 'G', 'A'].__contains__(natural_notes[note]):
+             semitones = f'{int(semitones[0]) - 2}st'
+         elif second_note == 'E':
+             semitones = f'{int(semitones[0] - 1)}st'
+         else:
+           # Restart loop
+           note = natural_notes.index(first_note[0])
+           # Count one octave more
+           octave += 1
+    # Only work for ascendent intervals       
+    return second_note
